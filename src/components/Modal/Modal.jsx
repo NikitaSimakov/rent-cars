05530b12493f7676aff6b0c1f121764aa45bc9b6
NikitaSimakov <mishaaniskin@gmail.com>
2023-09-18 10:42:22 +0300

    add link to phone in modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,19 @@
 import { AiOutlineClose } from 'react-icons/ai';
 import css from './Modal.module.css';
 export const Modal = ({ card, setIsModalOpen }) => {
-  const { id, make, model, year, type, mileage, img, rentalPrice } = card[0];
+  const {
+    id,
+    make,
+    model,
+    year,
+    type,
+    mileage,
+    img,
+    rentalPrice,
+    description,
+    accessories,
+    rentalConditions,
+  } = card[0];
 
   const handleBackdropClick = event => {
     if (event.target.id !== 'backdrop') return;
@@ -14,13 +26,30 @@ export const Modal = ({ card, setIsModalOpen }) => {
       id="backdrop"
     >
       <div className={css.modalWrapper} key={id} id="modal">
-        <img src={img} alt={model} width="400px" />
-        <h1>
+        <img className={css.img} src={img} alt={model} width="400px" />
+        <div className={css.title}>
+          <h2>{make}</h2> <h2 className={css.model}>{model}</h2> <h2>{year}</h2>
+        </div>
+        {/* <h1>
           {make} {model}, {year}
-        </h1>
+        </h1> */}
         <p>
           {type} {mileage} {rentalPrice}
         </p>
+        <p className={css.description}>{description}</p>
+        <p className={css.accessories}>Accessories and functionalities: </p>
+        <div className={css.accessoriesBox}>
+          {accessories.map(acces => (
+            <p className={css.accessory}>{acces}</p>
+          ))}
+        </div>
+        <div className={css.rentalConditions}>
+          <p>Rental conditions:</p>
+          <p>{rentalConditions}</p>
+        </div>
+        <a className={css.rentalLink} href="tel:+380730000000">
+          Rental Car
+        </a>
         <button
           onClick={() => setIsModalOpen(false)}
           className={css.closeBtn}
@@ -31,5 +60,4 @@ export const Modal = ({ card, setIsModalOpen }) => {
       </div>
     </div>
   );
-  // });
 };
